9f769864bffe3cc55b9637408b693b2d07918e24
ivanchikap <anton.ivanchik@gmail.com>
2020-10-12 16:57:11 +0300

    hw3

---
+++ hw3/app.js
@@ -0,0 +1,18 @@
+// 1. Создать обьект с полем product равным iphone
+const obj = {
+    product: 'iphone'
+};
+
+// 2. Добавить поле price = 1000 и currency = 'dollar'
+
+obj.price = 1000;
+obj.currency = 'dollar';
+
+// 3. Добавьте поле details, которое будет содержать обьект с полями model, color\
+
+obj.details = {
+    model: '2s',
+    collor: 'blue'
+}
+
+console.log(obj);
